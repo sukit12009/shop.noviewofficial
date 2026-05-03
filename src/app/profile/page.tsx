c70960088852cf0edad16e774d264189ee286452
Sukit Chowandee <sukit.c@teemasolution.io>
2026-05-04 01:20:27 +0700

    fix: for buile deploy

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from 'react';
 import { ProfilePage } from '@/modules/profile/components/ProfilePage';
 
 export const metadata = {
@@ -5,5 +6,9 @@ export const metadata = {
 };
 
 export default function Page() {
-  return <ProfilePage />;
+  return (
+    <Suspense>
+      <ProfilePage />
+    </Suspense>
+  );
 }
